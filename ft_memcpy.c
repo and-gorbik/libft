59a0f83c702d53b342d412155d95282a9c52ad14
Savannah Jacelyn bywater <sjacelyn@e1r11p3.21-school.ru>
2018-12-08 15:51:37 +0300

    add a check for equal strings

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sjacelyn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 12:51:02 by sjacelyn          #+#    #+#             */
-/*   Updated: 2018/11/29 15:14:15 by sjacelyn         ###   ########.fr       */
+/*   Updated: 2018/12/06 16:48:03 by sjacelyn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
 
+	if (dst == src)
+		return (dst);
 	i = 0;
 	while (i < n)
 	{
