a7ffdfd56e1f2a5db17675ea5f723aef99140da5
Andrey Gorbik <and-gorbik@mail.ru>
2018-12-15 14:41:36 +0300

    add zero check for *alst

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -16,6 +16,8 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	if (!alst || !del)
 		return ;
+	if (!*alst)
+		return ;
 	(*del)((*alst)->content, (*alst)->content_size);
 	free(*alst);
 	*alst = NULL;
